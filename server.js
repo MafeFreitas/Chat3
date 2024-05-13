58ec80b6122ac70143153eeae568b0a88c239d84
MafeFreitas <144139738+MafeFreitas@users.noreply.github.com>
2024-05-13 20:05:44 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+
+//Importar os módulos FRAMEWORKS
+
+const express = require('express');
+
+const app = require('express')();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+//Rota para a página principal
+app.get('/', (req,res) => res.sendFile(__dirname + '/public/index.html'));
+
+ app.use(express.static('public'))
+
+//Evento para quando o usuário conectar ao servidor
+io.on('connection', (socket) => {
+    console.log('Usuário conectado')
+
+    //Evento para quando envia uma mensagem
+    socket.on('chat message', (data) => io.emit('chat message', data));
+
+    //Evento para quando o usuário desconectar
+    socket.on('disconnect', () => console.log('Usuário desconectado'));
+});
+
+//Iniciar o servidor
+http.listen(3000,() => {
+    console.log(`Servidor rodando - Link http://localhost:3000`)
+})
+
